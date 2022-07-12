feee83dc9f4d8f8e81e0af323e477c435e40489e
James Wu <jameswu62@yahoo.co.nz>
2022-07-12 21:28:54 +1200

    add test file

--- test.py
+++ test.py
@@ -1,26 +1,10 @@
-from datetime import datetime
-import time
-
 class Test:
-    def __init__(self) -> None:
-        self._count = 0
+    def __init__(self, num: int):
+        self.val = num
+    
+    def printNum(self):
+        print(f'value is: {self.val}')
 
-    def increase(self) -> None:
-        self._count = self._count + 1
 
-    def printCount(self) -> None:
-        print(f'count is {self._count}')
-
-t = Test()
-
-t.increase()
-t.printCount()
-
-start = time.time()
-
-loop = 0xffff
-while(loop != 0):
-    loop = loop -1
-diff = time.time() - start
-
-print(diff)
+test = Test(10)
+test.printNum()
